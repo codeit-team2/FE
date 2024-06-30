a3348bcd32484a1abc7230ba9b8a541da830d71d
JunCastle <juncastle97@gmail.com>
2024-06-30 09:14:03 +0900

    feat : px설정 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,11 @@
 import type { Config } from 'tailwindcss';
 
+const px0_10 = Object.fromEntries(Array.from({ length: 11 }, (_, i) => [i, `${i}px`]));
 const px0_50 = Object.fromEntries(Array.from({ length: 51 }, (_, i) => [i, `${i}px`]));
 const px0_100 = Object.fromEntries(Array.from({ length: 101 }, (_, i) => [i, `${i}px`]));
 const px0_200 = Object.fromEntries(Array.from({ length: 201 }, (_, i) => [i, `${i}px`]));
 const px0_400 = Object.fromEntries(Array.from({ length: 401 }, (_, i) => [i, `${i}px`]));
+const px0_1000 = Object.fromEntries(Array.from({ length: 1001 }, (_, i) => [i, `${i}px`]));
 
 const config: Config = {
   content: [
@@ -13,10 +15,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      borderRadius: px0_50,
+      borderWidth: px0_50,
+      borderRadius: px0_100,
+      lineHeight: px0_100,
       fontSize: px0_100,
-      spacing: px0_200,
-      width: px0_400,
+      spacing: px0_400,
+      minWidth: px0_400,
+      minHeight: px0_400,
+      width: px0_1000,
+      height: px0_1000,
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
