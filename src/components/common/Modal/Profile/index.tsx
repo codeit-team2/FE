4834c52c0027d9d7a97f8f288ebd1ee865090356
marchfirst01 <marchfirst3113@gmail.com>
2024-07-01 16:06:53 +0900

    feat: Profile modal UI

---
+++ src/components/common/Modal/Profile/index.tsx
@@ -0,0 +1,32 @@
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import React from 'react';
+
+export default function ProfileModal() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Profile</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-520 flex flex-col gap-24 p-24">
+        <DialogHeader>
+          <DialogTitle>프로필 수정하기</DialogTitle>
+        </DialogHeader>
+        <DialogHeader>
+          <DialogTitle>회사</DialogTitle>
+          <input className="bg-gray-50" />
+        </DialogHeader>
+        <div className="flex w-full justify-center">
+          <Button>취소</Button>
+          <Button type="submit">수정하기</Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import ReviewModal from '@/components/common';
+import ProfileModal from '@/components/common/Modal/Profile';
 import { Button } from '@/components/ui/button';
 
 function Main() {
@@ -7,6 +8,7 @@ function Main() {
       <div className="mb-10 flex w-full rounded-sm bg-red-100">dd</div>
       <Button>Button</Button>
       <ReviewModal />
+      <ProfileModal />
     </>
   );
 }
