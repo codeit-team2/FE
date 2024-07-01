41b6727e9c060da7c9e02c6bfa02fc2053ddc970
JunCastle <juncastle97@gmail.com>
2024-07-01 19:05:49 +0900

    feat : 버튼 공용 컴포넌트화

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,6 @@
-import { Button } from "@/components/ui/button"
-
 function Main() {
   return (
-    <>
-      <div className="mb-10 flex w-full rounded-sm bg-red-100">dd</div>
-      <Button>Button</Button>
-    </>
+    <></>
   );
 }
 
