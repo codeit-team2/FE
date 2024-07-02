647bb871786e01e8dd5a23447f90e470d731ea77
marchfirst01 <marchfirst3113@gmail.com>
2024-07-02 11:46:42 +0900

    feat: Login Status 추가

--- src/components/common/GNB/index.tsx
+++ src/components/common/GNB/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Image from 'next/image';
 
 export default function GNB() {
+  // api 연동하면서 수정필요
+  const isloginStatus = true;
+  const likeItems = 10;
+
   return (
     <div className="shadow-gnbShadow flex w-full flex-row justify-between px-32 py-16">
       <Image src="/icons/Logo.svg" alt="logo" priority={true} width={65} height={14} />
@@ -12,9 +16,21 @@ export default function GNB() {
         <a href="/" className="font-Pretendard text-base font-medium text-gray-500">
           모든 리뷰
         </a>
-        <a href="/" className="font-Pretendard text-base font-medium text-gray-500">
-          로그인
-        </a>
+        {isloginStatus ? (
+          <>
+            <div className="flex flex-row gap-4">
+              <a href="/" className="font-Pretendard text-base font-medium text-gray-500">
+                찜한 모임
+              </a>
+              {likeItems ? <p className="text-blue-600">{likeItems}</p> : ''}
+            </div>
+            <Image src="/icons/profile.svg" alt="profile" width={32} height={32} />
+          </>
+        ) : (
+          <a href="/" className="font-Pretendard text-base font-medium text-gray-500">
+            로그인
+          </a>
+        )}
       </div>
     </div>
   );
