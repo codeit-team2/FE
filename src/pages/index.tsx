cd429db3e276295f60a12c7c55cfaacf7a78742c
JunCastle <juncastle97@gmail.com>
2024-07-02 17:48:22 +0900

    chore : main 페이지 복구

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,6 @@
-import Card from '@/components/Card';
-import mockData from "@/components/Card/testData.json"
-
 function Main() {
   return (
-    <div className="h-screen w-full bg-gray-100 p-30">
-      {mockData.map((data, index) => (
-        <Card key={index} data={data} />
-      ))}
-    </div>
+    <></>
   );
 }
 
