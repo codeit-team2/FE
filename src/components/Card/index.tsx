6d7daad30530501f55462a3b85f60c256e243fed
JunCastle <juncastle97@gmail.com>
2024-07-02 17:46:33 +0900

    feat : card 컴포넌트 구현

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,47 +2,58 @@ import Image from 'next/image';
 import React, { useState } from 'react';
 import { Button } from '../ui/button';
 
-export default function Card() {
+export default function Card({ data }: any) {
   const [isBookmarked, setIsBookmarked] = useState(false);
 
   const handleClick = () => {
     setIsBookmarked((prevIsBookmarked) => !prevIsBookmarked);
   };
 
+  const progressPercentage = (data.member / 20) * 100;
+
   return (
-    <>
-      <div className="relative flex h-230 w-[1216px] gap-20 rounded-30 bg-white p-20">
-        <div className="relative h-190 w-373">
-          <Image src={'/images/러닝이미지.jpg'} alt="러닝이미지" fill className="rounded-20" />
-        </div>
-        <div className="relative text-gray-600">
-          <div>
-            <div className="flex gap-6">
-              <p className="text-secondary-blue-300">러닝</p>
-              <p>중랑구</p>
-            </div>
-            <div className="flex gap-6">
-              <p className="text-secondary-red-300">오늘마감</p>·<p>7월 2일 화요일</p>·<p>오전 11:11</p>
-            </div>
-            <div className="text-24 font-medium text-black">중랑천 함께 뛰어요</div>
+    <div className="relative flex h-230 w-[1216px] gap-20 rounded-30 bg-white p-20">
+      <div className="relative h-190 w-373">
+        <Image src={data.imageUrl} alt={data.title} fill className="rounded-20" />
+        {data.confirmed && (
+          <div className="bg-secondary-red-300 absolute flex h-36 w-81 items-center justify-center rounded-br-20 rounded-tl-20 text-14 text-white">
+            개설확정
           </div>
-          <div className="absolute bottom-11 flex items-center justify-center gap-16">
-            <div className="text-14">18/20</div>
-            <div className="h-6 w-422 rounded-full bg-gray-100"></div>
+        )}
+      </div>
+      <div className="relative text-gray-600">
+        <div>
+          <div className="flex gap-6">
+            <p className="text-secondary-blue-300">{data.category}</p>
+            <p>{data.place}</p>
           </div>
+          <div className="flex gap-6">
+            <p className="text-secondary-red-300">{data.deadline}</p>·<p>{data.date}</p>·
+            <p>{data.time}</p>
+          </div>
+          <div className="text-24 font-medium text-black">{data.title}</div>
         </div>
-        <Button className="absolute bottom-19 right-20 h-42 w-288">참여하기</Button>
-        <div className='absolute top-26 right-26'>
-          <button type="button" className="relative h-48 w-48" onClick={handleClick}>
-            <Image
-              src={isBookmarked ? '/icons/heart-pink.svg' : '/icons/heart-gray.svg'}
-              alt="찜 버튼"
-              fill
-              className="absolute"
+        <div className="absolute bottom-11 flex items-center justify-center gap-16">
+          <div className="text-14">{data.member}/20</div>
+          <div className="relative h-6 w-422 rounded-full bg-gray-100">
+            <div
+              className="bg-secondary-blue-300 absolute h-6 rounded-full"
+              style={{ width: `${progressPercentage}%` }}
             />
-          </button>
+          </div>
         </div>
       </div>
-    </>
+      <Button className="absolute bottom-19 right-20 h-42 w-288">참여하기</Button>
+      <div className="absolute right-26 top-26">
+        <button type="button" className="relative h-48 w-48" onClick={handleClick}>
+          <Image
+            src={isBookmarked ? '/icons/heart-pink.svg' : '/icons/heart-gray.svg'}
+            alt="찜 버튼"
+            fill
+            className="absolute"
+          />
+        </button>
+      </div>
+    </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import Card from '@/components/Card';
+import mockData from "@/components/Card/testData.json"
 
 function Main() {
   return (
     <div className="h-screen w-full bg-gray-100 p-30">
-      <Card />
+      {mockData.map((data, index) => (
+        <Card key={index} data={data} />
+      ))}
     </div>
   );
 }
