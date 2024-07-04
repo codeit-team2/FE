fd80f9d3c4ddff5471983d95d3bcd04ce47293da
JunCastle <juncastle97@gmail.com>
2024-07-04 14:47:18 +0900

    feat : 리뷰 카드 구현

---
+++ src/components/Detail/ReviewCard/index.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import React from 'react';
+
+export default function ReviewCard({ data }: any) {
+  return (
+    <div className="flex h-158 w-494 flex-col justify-between rounded-md bg-white px-20 py-16">
+      <div className='flex flex-col gap-6'>
+        <div className="relative flex">
+          {[...Array(5)].map((_, i) => (
+            <div key={i} className="relative h-24 w-24">
+              <Image src={'/icons/star-empty.svg'} alt={'Empty star'} fill />
+            </div>
+          ))}
+          <div className="absolute left-0 top-0 flex">
+            {[...Array(data.star)].map((_, i) => (
+              <div key={i} className="relative h-24 w-24">
+                <Image src={'/icons/star-filled.svg'} alt={'Filled star'} fill />
+              </div>
+            ))}
+          </div>
+        </div>
+        <p className="text-body-1Sb text-neutral-900">{data.comment}</p>
+      </div>
+      <div className="flex items-center gap-8 pt-14">
+        <div className="relative h-32 w-32 overflow-hidden rounded-full">
+          <Image src={'/images/profileimage.jpeg'} alt="profileImage" fill />
+        </div>
+        <p className="text-body-2M text-neutral-600">{data.nickname}</p>
+        <div className="h-16 w-1 rounded-full bg-neutral-200"></div>
+        <p className="text-body-2M text-neutral-400">{data.date}</p>
+      </div>
+    </div>
+  );
+}
