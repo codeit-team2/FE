97c0ba47dd3dc4d38e998c1674a25ed616538f9a
JunCastle <juncastle97@gmail.com>
2024-07-04 14:47:45 +0900

    feat : 플로팅바 구현

--- src/components/Detail/FloatingBar/index.tsx
+++ src/components/Detail/FloatingBar/index.tsx
@@ -1,7 +1,10 @@
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 import React, { useState } from 'react';
 
 export default function FloatingBar({ data }: any) {
+  const isOwner = false;
+
   const maxReached = data.member >= 20;
 
   const [isEntered, setIsEntered] = useState(false);
@@ -12,12 +15,19 @@ export default function FloatingBar({ data }: any) {
 
   return (
     <div className="fixed bottom-0 flex h-74 w-full items-center justify-center border border-t-neutral-100 py-16 backdrop-blur-sm">
-      <Button
-        className="w-392"
-        onClick={handleClick}
-        disabled={!isEntered && maxReached}
-        children={isEntered ? '참여 취소하기' : '참여하기'}
-      />
+      {isOwner ? (
+        <div className='flex items-center justify-center gap-12'>
+          <Button className="w-392">개설 취소하기</Button>
+          <Image src={'/icons/share-button.svg'} alt='share-button' width={42} height={42}/>
+        </div>
+      ) : (
+        <Button
+          className="w-392"
+          onClick={handleClick}
+          disabled={!isEntered && maxReached}
+          children={isEntered ? '참여 취소하기' : '참여하기'}
+        />
+      )}
     </div>
   );
 }
