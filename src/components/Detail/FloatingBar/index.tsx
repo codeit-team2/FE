f465039a80f57bdb5bb10c9dd238f74c836c7b4b
JunCastle <juncastle97@gmail.com>
2024-07-04 17:45:25 +0900

    feat : LoginRequired 모달 구현

--- src/components/Detail/FloatingBar/index.tsx
+++ src/components/Detail/FloatingBar/index.tsx
@@ -1,3 +1,4 @@
+import LoginRequired from '@/components/common/Modal/LoginRequired';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import React, { useState } from 'react';
@@ -8,26 +9,57 @@ export default function FloatingBar({ data }: any) {
   const maxReached = data.member >= 20;
 
   const [isEntered, setIsEntered] = useState(false);
+  const [showLoginModal, setShowLoginModal] = useState(false);
+
+  const isLoggedIn = false;
 
   const handleClick = () => {
-    setIsEntered((prevIsEntered) => !prevIsEntered);
+    if (!isLoggedIn) {
+      setShowLoginModal(true);
+    } else {
+      setIsEntered((prevIsEntered) => !prevIsEntered);
+    }
+  };
+
+  const handleCopyURL = async () => {
+    const url = window.location.href;
+    try {
+      await navigator.clipboard.writeText(url);
+      alert('링크가 복사되었습니다.😆');
+    } catch (err) {
+      console.error('링크 복사에 실패했습니다.🥲', err);
+    }
+  };
+
+  const handleCloseLoginModal = () => {
+    setShowLoginModal(false);
   };
 
   return (
-    <div className="fixed bottom-0 flex h-74 w-full items-center justify-center border border-t-neutral-100 py-16 backdrop-blur-sm">
-      {isOwner ? (
-        <div className='flex items-center justify-center gap-12'>
-          <Button className="w-392">개설 취소하기</Button>
-          <Image src={'/icons/share-button.svg'} alt='share-button' width={42} height={42}/>
-        </div>
-      ) : (
-        <Button
-          className="w-392"
-          onClick={handleClick}
-          disabled={!isEntered && maxReached}
-          children={isEntered ? '참여 취소하기' : '참여하기'}
-        />
+    <>
+      {showLoginModal && (
+        <>
+          <div className="fixed inset-0 z-40 bg-neutral-900 opacity-70"></div>
+          <LoginRequired onClose={handleCloseLoginModal} />
+        </>
       )}
-    </div>
+      <div className="fixed bottom-0 flex h-74 w-full items-center justify-center border border-t-neutral-100 py-16 backdrop-blur-sm">
+        {isOwner ? (
+          <div className="flex items-center justify-center gap-12">
+            <Button className="w-392">개설 취소하기</Button>
+            <button onClick={handleCopyURL}>
+              <Image src={'/icons/share-button.svg'} alt="share-button" width={42} height={42} />
+            </button>
+          </div>
+        ) : (
+          <Button
+            className="w-392"
+            onClick={handleClick}
+            disabled={!isEntered && maxReached}
+            children={isEntered ? '참여 취소하기' : '참여하기'}
+          />
+        )}
+      </div>
+    </>
   );
 }
