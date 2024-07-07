cd8b7d861256258717af395763ba1da32fda4597
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-07-07 16:23:27 +0900

    feat: Login 컴포넌트 구현

--- src/components/common/Modal/Login/index.tsx
+++ src/components/common/Modal/Login/index.tsx
@@ -1,15 +1,21 @@
-import { useEffect, useState } from 'react';
+import { Dispatch, SetStateAction } from 'react';
 import { useForm, SubmitHandler, FieldValues, FormProvider } from 'react-hook-form';
 import { ERROR_MESSAGE, PLACEHOLDER } from '@/constants/formMessages';
 import Input from '@/components/common/Input';
 import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
-import Signup from '@/components/common/Modal/Signup';
 
-export default function Login({ loginOpen, setLoginOpen }) {
-  const [dialogopen, setDialogOpen] = useState(loginOpen); //현재 모달 상태
-  const [signupOpen, setSignupOpen] = useState(false); // 회원가입 모달 상태
+interface LoginProps {
+  isLoginModalOpen: boolean;
+  setIsLoginModalOpen: Dispatch<SetStateAction<boolean>>;
+  setIsSignupModalOpen: Dispatch<SetStateAction<boolean>>;
+}
 
+export default function Login({
+  isLoginModalOpen,
+  setIsLoginModalOpen,
+  setIsSignupModalOpen,
+}: LoginProps) {
   const form = useForm();
 
   const {
@@ -20,99 +26,74 @@ export default function Login({ loginOpen, setLoginOpen }) {
 
   const onSubmit: SubmitHandler<FieldValues> = (value: FieldValues) => {};
 
-  useEffect(() => {
-    if (!dialogopen && setLoginOpen) {
-      setLoginOpen(false);
-    }
-    // if (open2 != undefined) {
-    setDialogOpen(loginOpen);
-    // }
-  }, [dialogopen, setLoginOpen]);
-
-  // useEffect(() => {
-  //   if (setLoginOpen) {
-  //     if (open) {
-  //       setDialogOpen(true);
-  //     }
-  //     if (dialogopen) {
-  //       setLoginOpen(false);
-  //     }
-  //   }
-  // }, [open, dialogopen]);
-
-  // console.log(signupOpen);
-
   return (
-    <>
-      <Dialog open={dialogopen} onOpenChange={setDialogOpen}>
-        <DialogTrigger>
-          <div>로그인</div>
-        </DialogTrigger>
-        <DialogContent className="w-520">
-          <DialogTitle>로그인</DialogTitle>
-          <FormProvider {...form}>
-            <form
-              className="flex flex-col gap-50"
-              autoComplete="off"
-              onSubmit={handleSubmit(onSubmit)}
-            >
-              <div>
-                <label className="mb-6 block text-body-2Sb" htmlFor="email">
-                  이메일
-                </label>
-                <div className="flex gap-8">
-                  <Input
-                    type="text"
-                    id="email"
-                    placeholder={PLACEHOLDER.email}
-                    {...register('email', {
-                      required: ERROR_MESSAGE.email.required,
-                    })}
-                  />
-                </div>
+    <Dialog open={isLoginModalOpen} onOpenChange={setIsLoginModalOpen}>
+      <DialogTrigger>
+        <div>로그인</div>
+      </DialogTrigger>
+      <DialogContent className="px-20 pb-50 pt-32 sm:w-520 sm:px-40 sm:pb-32">
+        <DialogTitle>로그인</DialogTitle>
+        <FormProvider {...form}>
+          <form
+            className="flex flex-col gap-50"
+            autoComplete="off"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div>
+              <label className="mb-6 block text-body-2Sb" htmlFor="email">
+                이메일
+              </label>
+              <div className="flex gap-8">
+                <Input
+                  type="text"
+                  id="email"
+                  placeholder={PLACEHOLDER.email}
+                  {...register('email', {
+                    required: ERROR_MESSAGE.email.required,
+                  })}
+                />
               </div>
-              <div>
-                <label className="mb-6 block text-body-2Sb" htmlFor="password">
-                  비밀번호
-                </label>
-                <div className="flex gap-8">
-                  <Input
-                    type="password"
-                    id="password"
-                    placeholder={PLACEHOLDER.password}
-                    maxLength={100}
-                    {...register('password', {
-                      required: ERROR_MESSAGE.password.required,
-                    })}
-                  />
-                </div>
+            </div>
+            <div>
+              <label className="mb-6 block text-body-2Sb" htmlFor="password">
+                비밀번호
+              </label>
+              <div className="flex gap-8">
+                <Input
+                  type="password"
+                  id="password"
+                  placeholder={PLACEHOLDER.password}
+                  maxLength={100}
+                  {...register('password', {
+                    required: ERROR_MESSAGE.password.required,
+                  })}
+                />
               </div>
-              <div>
-                <Button
-                  className={`mb-24 w-full ${!isValid && 'cursor-default bg-neutral-400 !text-neutral-100 hover:!text-neutral-100'}`}
-                  variant="secondary"
-                  type="submit"
+            </div>
+            <div>
+              <Button
+                className={`mb-24 w-full ${!isValid && 'cursor-default bg-neutral-400 !text-neutral-100 hover:!text-neutral-100'}`}
+                variant="secondary"
+                type="submit"
+              >
+                로그인
+              </Button>
+              <p className="text-center text-body-2Sb">
+                <span className="mr-6">취ZONE이 처음이신가요?</span>
+                <span
+                  className="cursor-pointer text-primary-300 underline underline-offset-2"
+                  onClick={() => {
+                    setIsSignupModalOpen(true);
+                    setIsLoginModalOpen(false);
+                  }}
                 >
-                  로그인
-                </Button>
-                <p className="text-center text-body-2Sb">
-                  <span className="mr-6">취ZONE이 처음이신가요?</span>
-                  <span
-                    className="cursor-pointer text-primary-300 underline underline-offset-2"
-                    onClick={() => {
-                      setSignupOpen(true);
-                      setDialogOpen(false);
-                    }}
-                  >
-                    회원가입
-                  </span>
-                </p>
-              </div>
-            </form>
-          </FormProvider>
-        </DialogContent>
-      </Dialog>
-      {signupOpen && <Signup loginOpen2={loginOpen} signupOpen={signupOpen} setSignupOpen={setSignupOpen} />}
-    </>
+                  회원가입
+                </span>
+              </p>
+            </div>
+          </form>
+        </FormProvider>
+      </DialogContent>
+    </Dialog>
   );
 }
