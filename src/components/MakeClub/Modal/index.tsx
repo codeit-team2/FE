9888f959b1d2c937ba08de07b7ea7fad50e8ebfd
marchfirst01 <marchfirst3113@gmail.com>
2024-07-08 10:38:49 +0900

    fix: merge conflict 해결 - button, calendar ...

--- src/components/MakeClub/Modal/index.tsx
+++ src/components/MakeClub/Modal/index.tsx
@@ -7,11 +7,15 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog';
-import { FaPlus } from 'react-icons/fa6';
-import { Input } from '@/components/common/Input';
+import Input from '@/components/common/Input';
 import Calendar from '@/components/common/Calendar';
+import { FieldValues, FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 
-export default function MakeClubModal() {
+interface Props {
+  trigger: 'text' | 'plus';
+}
+
+export default function MakeClubModal({ trigger }: Props) {
   const amTime = ['09:00', '10:00', '11:00', '12:00'];
   const pmTime = [
     '13:00',
@@ -25,107 +29,123 @@ export default function MakeClubModal() {
     '21:00',
     '22:00',
   ];
-  const [selectTime, setSelectTime] = useState<string[]>([]);
-
-  const handleTimeClick = (time: string) => {
-    setSelectTime((prev) => {
-      if (prev.includes(time)) {
-        return prev.filter((t) => t !== time);
-      } else {
-        return [...prev, time];
-      }
-    });
-  };
+  const [selectTime, setSelectTime] = useState<string>();
 
   const fileInput = React.useRef<HTMLInputElement | null>(null);
   const handleButtonClick = () => {
     fileInput.current?.click();
   };
 
+  const triggerButton = trigger === 'text' ? <Button>모임만들기</Button> : <Button>Plus</Button>;
+
+  const form = useForm();
+
+  const {
+    handleSubmit,
+    register,
+    formState: { isValid },
+  } = form;
+
+  const onSubmit: SubmitHandler<FieldValues> = (value: FieldValues) => {};
+
   return (
     <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="default">
-          <FaPlus />
-        </Button>
-      </DialogTrigger>
+      <DialogTrigger asChild>{triggerButton}</DialogTrigger>
       <DialogContent className="flex flex-col items-center gap-24 px-40 py-32">
         <DialogTitle className="w-952 text-center">모임 만들기</DialogTitle>
         <div className="flex w-full flex-row justify-center">
-          <div className="flex w-full flex-col gap-24">
-            <div>
-              <DialogDescription>대표 이미지</DialogDescription>
-              <div className="flex flex-row items-center gap-8">
-                <input
-                  type="file"
-                  ref={fileInput}
-                  className="block rounded-sm bg-neutral-50 px-12 py-10 file:hidden"
-                />
-                <Button variant="chip" selected={true} onClick={() => handleButtonClick()}>
-                  파일찾기
-                </Button>
+          <FormProvider {...form}>
+            <form
+              className="flex flex-col gap-50"
+              autoComplete="off"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <div className="flex w-full flex-col gap-24">
+                <div>
+                  <DialogDescription>대표 이미지</DialogDescription>
+                  <div className="flex flex-row items-center gap-8">
+                    <input
+                      type="file"
+                      ref={fileInput}
+                      className="block rounded-sm bg-neutral-50 px-12 py-10 file:hidden"
+                    />
+                    <Button variant="chip" selected={true} onClick={() => handleButtonClick()}>
+                      파일찾기
+                    </Button>
+                  </div>
+                </div>
+                <div className="flex flex-row gap-12">
+                  <div className="w-3/6">
+                    <DialogDescription>카테고리</DialogDescription>
+                    <Input type="text" id="category" placeholder="카테고리를 선택해주세요" />
+                  </div>
+                  <div className="w-3/6">
+                    <DialogDescription>지역</DialogDescription>
+                    <Input type="text" id="location" placeholder="지역을 선택해주세요" />
+                  </div>
+                </div>
+                <div>
+                  <DialogDescription>날짜</DialogDescription>
+                  <div className="mx-auto w-full rounded-md border">
+                    <Calendar />
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="flex flex-row gap-12">
-              <div className="w-3/6">
-                <DialogDescription>카테고리</DialogDescription>
-                <Input type="text" placeholder="카테고리를 선택해주세요" />
+              <div className="mx-32 border-l"></div>
+              <div className="flex w-full flex-col gap-24">
+                <div>
+                  <DialogDescription>오전</DialogDescription>
+                  <div className="flex gap-4">
+                    {amTime.map((time, i) => (
+                      <Button
+                        variant="chip"
+                        size="chip"
+                        key={i}
+                        selected={selectTime === time}
+                        onClick={() => setSelectTime(time)}
+                      >
+                        {time}
+                      </Button>
+                    ))}
+                  </div>
+                </div>
+                <div>
+                  <DialogDescription>오후</DialogDescription>
+                  <div className="flex flex-wrap gap-4">
+                    {pmTime.map((time, i) => (
+                      <Button
+                        variant="chip"
+                        size="chip"
+                        key={i}
+                        selected={selectTime === time}
+                        onClick={() => setSelectTime(time)}
+                      >
+                        {time}
+                      </Button>
+                    ))}
+                  </div>
+                </div>
+                <div>
+                  <DialogDescription>모임명</DialogDescription>
+                  <Input
+                    type="text"
+                    id="clubName"
+                    placeholder="30자 이내로 모임명을 입력해주세요"
+                    maxLength={30}
+                  />
+                </div>
+                <div>
+                  <DialogDescription>모임 정원</DialogDescription>
+                  <Input
+                    type="text"
+                    id="number"
+                    placeholder="5에서 20까지 숫자로만 입력해주세요"
+                    maxLength={20}
+                  />
+                </div>
               </div>
-              <div className="w-3/6">
-                <DialogDescription>지역</DialogDescription>
-                <Input type="text" placeholder="지역을 선택해주세요" />
-              </div>
-            </div>
-            <div>
-              <DialogDescription>날짜</DialogDescription>
-              <div className="mx-auto w-full rounded-md border">
-                <Calendar />
-              </div>
-            </div>
-          </div>
-          <div className="mx-32 border-l"></div>
-          <div className="flex w-full flex-col gap-24">
-            <div>
-              <DialogDescription>오전</DialogDescription>
-              <div className="flex gap-4">
-                {amTime.map((time, i) => (
-                  <Button
-                    variant="chip"
-                    size="chip"
-                    key={i}
-                    selected={selectTime.includes(time)}
-                    onClick={() => handleTimeClick(time)}
-                  >
-                    {time}
-                  </Button>
-                ))}
-              </div>
-            </div>
-            <div>
-              <DialogDescription>오후</DialogDescription>
-              <div className="flex flex-wrap gap-4">
-                {pmTime.map((time, i) => (
-                  <Button
-                    variant="chip"
-                    size="chip"
-                    key={i}
-                    selected={selectTime.includes(time)}
-                    onClick={() => handleTimeClick(time)}
-                  >
-                    {time}
-                  </Button>
-                ))}
-              </div>
-            </div>
-            <div>
-              <DialogDescription>모임명</DialogDescription>
-              <Input type="text" placeholder="30자 이내로 모임명을 입력해주세요" maxLength={30} />
-            </div>
-            <div>
-              <DialogDescription>모임 정원</DialogDescription>
-              <Input type="text" placeholder="5에서 20까지 숫자로만 입력해주세요" maxLength={20} />
-            </div>
-          </div>
+            </form>
+          </FormProvider>
         </div>
         <div className="flex justify-center">
           <Button className="w-952">확인</Button>
--- src/components/NotCard/index.tsx
+++ src/components/NotCard/index.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button';
+import MakeClubModal from '../MakeClub/Modal';
 
 export default function NotCard() {
   const boxinnerStyle = 'rounded-[4px] bg-neutral-50';
@@ -8,13 +9,13 @@ export default function NotCard() {
         <div className="flex gap-20">
           <div className={`h-190 w-373 rounded-md bg-neutral-50`} />
           <div className="flex flex-col gap-12">
-            <div className={`w-126 h-16 ${boxinnerStyle}`} />
-            <div className={`w-187 h-16 ${boxinnerStyle}`} />
-            <div className={`w-394 h-32 ${boxinnerStyle}`} />
+            <div className={`h-16 w-126 ${boxinnerStyle}`} />
+            <div className={`h-16 w-187 ${boxinnerStyle}`} />
+            <div className={`h-32 w-394 ${boxinnerStyle}`} />
           </div>
         </div>
-        <div className={`h-54 w-54 right-30 top-30 absolute rounded-full bg-neutral-50`} />
-        <div className={`h-47 w-278 absolute bottom-20 right-20 ${boxinnerStyle}`} />
+        <div className={`absolute right-30 top-30 h-54 w-54 rounded-full bg-neutral-50`} />
+        <div className={`absolute bottom-20 right-20 h-47 w-278 ${boxinnerStyle}`} />
       </div>
 
       <p className="mb-12 text-center text-neutral-500">
@@ -22,7 +23,8 @@ export default function NotCard() {
         <br></br>
         지금 바로 모임을 만들어보세요
       </p>
-      <Button>모임 만들기</Button>
+      <MakeClubModal trigger="plus" />
+      {/* <Button>모임 만들기</Button> */}
     </>
   );
 }
