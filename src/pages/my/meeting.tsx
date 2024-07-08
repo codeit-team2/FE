a84826edd6ad06d48e30138ad9431a744c42483f
marchfirst01 <marchfirst3113@gmail.com>
2024-07-08 18:27:41 +0900

    feat: my - tab component

---
+++ src/pages/my/meeting.tsx
@@ -0,0 +1,23 @@
+import Card from '@/components/Card';
+import Test from '@/components/Card/testData';
+import NotCard from '@/components/NotCard';
+import React from 'react';
+
+export default function Meeting() {
+  const TESTS = Test;
+  return (
+    <div>
+      <div className="flex flex-col gap-20 pb-50">
+        {TESTS ? (
+          <>
+            {Test.map((data, index) => (
+              <Card key={index} data={data} />
+            ))}
+          </>
+        ) : (
+          <NotCard />
+        )}
+      </div>
+    </div>
+  );
+}
