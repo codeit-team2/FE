73377d3cbe9098ea6ada77c0c9d06b9aa9ae23a7
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-07-08 17:33:35 +0900

    refactor: Banner variant 추가

--- src/components/common/Banner/index.tsx
+++ src/components/common/Banner/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 interface BannerProps {
+  variant?: 'default' | 'secondary';
   mainTitle: React.ReactNode;
   subTitle: string;
 }
 
-export default function Banner({ mainTitle, subTitle }: BannerProps) {
+export default function Banner({ variant = 'default', mainTitle, subTitle }: BannerProps) {
   return (
-    <div className="w-full rounded-lg bg-neutral-900 px-30 py-40 text-center md:px-36 md:py-44">
+    <div
+      className={`w-full rounded-lg px-30 py-40 text-center md:px-36 md:py-44 ${variant === 'default' ? 'bg-neutral-900' : 'bg-neutral-600'}`}
+    >
       <p className="mb-16 text-heading-2Sb text-white md:text-heading-1Sb">{mainTitle}</p>
       <p className="text-body-1Sb text-neutral-400 md:text-heading-2Sb">{subTitle}</p>
     </div>
