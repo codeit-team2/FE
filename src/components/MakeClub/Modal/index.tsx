5bbb2e865a4de2a7cc540e159a9e448685a19c32
marchfirst01 <marchfirst3113@gmail.com>
2024-07-08 11:27:42 +0900

    Design: 모임 만들기 모달 반응형 적용

--- src/components/MakeClub/Modal/index.tsx
+++ src/components/MakeClub/Modal/index.tsx
@@ -51,104 +51,102 @@ export default function MakeClubModal({ trigger }: Props) {
   return (
     <Dialog>
       <DialogTrigger asChild>{triggerButton}</DialogTrigger>
-      <DialogContent className="flex flex-col items-center gap-24 px-40 py-32">
-        <DialogTitle className="w-952 text-center">모임 만들기</DialogTitle>
-        <div className="flex w-full flex-row justify-center">
-          <FormProvider {...form}>
-            <form
-              className="flex flex-col gap-50"
-              autoComplete="off"
-              onSubmit={handleSubmit(onSubmit)}
-            >
-              <div className="flex w-full flex-col gap-24">
-                <div>
-                  <DialogDescription>대표 이미지</DialogDescription>
-                  <div className="flex flex-row items-center gap-8">
-                    <input
-                      type="file"
-                      ref={fileInput}
-                      className="block rounded-sm bg-neutral-50 px-12 py-10 file:hidden"
-                    />
-                    <Button variant="chip" selected={true} onClick={() => handleButtonClick()}>
-                      파일찾기
+      <DialogContent className="flex h-729 w-fit flex-col items-center gap-24 overflow-y-scroll px-40 py-32">
+        <DialogTitle className="w-460 text-center md:w-952">모임 만들기</DialogTitle>
+        <FormProvider {...form}>
+          <form
+            className="flex w-fit flex-col justify-center md:flex-row"
+            autoComplete="off"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div className="flex w-fit flex-col gap-24">
+              <div>
+                <DialogDescription>대표 이미지</DialogDescription>
+                <div className="flex flex-row items-center gap-8">
+                  <input
+                    type="file"
+                    ref={fileInput}
+                    className="block rounded-sm bg-neutral-50 px-12 py-10 file:hidden"
+                  />
+                  <Button variant="chip" selected={true} onClick={() => handleButtonClick()}>
+                    파일찾기
+                  </Button>
+                </div>
+              </div>
+              <div className="flex flex-row gap-12">
+                <div className="w-3/6">
+                  <DialogDescription>카테고리</DialogDescription>
+                  <Input type="text" id="category" placeholder="카테고리를 선택해주세요" />
+                </div>
+                <div className="w-3/6">
+                  <DialogDescription>지역</DialogDescription>
+                  <Input type="text" id="location" placeholder="지역을 선택해주세요" />
+                </div>
+              </div>
+              <div>
+                <DialogDescription>날짜</DialogDescription>
+                <div className="mx-auto w-fit rounded-md border">
+                  <Calendar />
+                </div>
+              </div>
+            </div>
+            <div className="mx-32 border-l"></div>
+            <div className="flex w-fit flex-col gap-24">
+              <div className="w-440">
+                <DialogDescription>오전</DialogDescription>
+                <div className="flex gap-4">
+                  {amTime.map((time, i) => (
+                    <Button
+                      variant="chip"
+                      size="chip"
+                      key={i}
+                      selected={selectTime === time}
+                      onClick={() => setSelectTime(time)}
+                    >
+                      {time}
                     </Button>
-                  </div>
-                </div>
-                <div className="flex flex-row gap-12">
-                  <div className="w-3/6">
-                    <DialogDescription>카테고리</DialogDescription>
-                    <Input type="text" id="category" placeholder="카테고리를 선택해주세요" />
-                  </div>
-                  <div className="w-3/6">
-                    <DialogDescription>지역</DialogDescription>
-                    <Input type="text" id="location" placeholder="지역을 선택해주세요" />
-                  </div>
-                </div>
-                <div>
-                  <DialogDescription>날짜</DialogDescription>
-                  <div className="mx-auto w-full rounded-md border">
-                    <Calendar />
-                  </div>
+                  ))}
                 </div>
               </div>
-              <div className="mx-32 border-l"></div>
-              <div className="flex w-full flex-col gap-24">
-                <div>
-                  <DialogDescription>오전</DialogDescription>
-                  <div className="flex gap-4">
-                    {amTime.map((time, i) => (
-                      <Button
-                        variant="chip"
-                        size="chip"
-                        key={i}
-                        selected={selectTime === time}
-                        onClick={() => setSelectTime(time)}
-                      >
-                        {time}
-                      </Button>
-                    ))}
-                  </div>
-                </div>
-                <div>
-                  <DialogDescription>오후</DialogDescription>
-                  <div className="flex flex-wrap gap-4">
-                    {pmTime.map((time, i) => (
-                      <Button
-                        variant="chip"
-                        size="chip"
-                        key={i}
-                        selected={selectTime === time}
-                        onClick={() => setSelectTime(time)}
-                      >
-                        {time}
-                      </Button>
-                    ))}
-                  </div>
-                </div>
-                <div>
-                  <DialogDescription>모임명</DialogDescription>
-                  <Input
-                    type="text"
-                    id="clubName"
-                    placeholder="30자 이내로 모임명을 입력해주세요"
-                    maxLength={30}
-                  />
-                </div>
-                <div>
-                  <DialogDescription>모임 정원</DialogDescription>
-                  <Input
-                    type="text"
-                    id="number"
-                    placeholder="5에서 20까지 숫자로만 입력해주세요"
-                    maxLength={20}
-                  />
+              <div className="w-440">
+                <DialogDescription>오후</DialogDescription>
+                <div className="flex flex-wrap gap-4">
+                  {pmTime.map((time, i) => (
+                    <Button
+                      variant="chip"
+                      size="chip"
+                      key={i}
+                      selected={selectTime === time}
+                      onClick={() => setSelectTime(time)}
+                    >
+                      {time}
+                    </Button>
+                  ))}
                 </div>
               </div>
-            </form>
-          </FormProvider>
-        </div>
+              <div>
+                <DialogDescription>모임명</DialogDescription>
+                <Input
+                  type="text"
+                  id="clubName"
+                  placeholder="30자 이내로 모임명을 입력해주세요"
+                  maxLength={30}
+                />
+              </div>
+              <div>
+                <DialogDescription>모임 정원</DialogDescription>
+                <Input
+                  type="text"
+                  id="number"
+                  placeholder="5에서 20까지 숫자로만 입력해주세요"
+                  maxLength={20}
+                />
+              </div>
+            </div>
+          </form>
+        </FormProvider>
         <div className="flex justify-center">
-          <Button className="w-952">확인</Button>
+          <Button className="w-460 md:w-952">확인</Button>
         </div>
       </DialogContent>
     </Dialog>
