dd06a0724e25f5db25eb2bdb6216f98a86471dbe
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-07-12 08:37:23 +0900

    feat: api 모듈화

---
+++ src/types/auths.ts
@@ -0,0 +1,23 @@
+export interface PostSignup {
+  email: string;
+  password: string;
+  nickname: string;
+}
+
+export interface PostSendmail {
+  email: string;
+}
+
+export interface PostVerify {
+  email: string;
+  verifyCode: string;
+}
+
+export interface PostNickname {
+  nickname: string;
+}
+
+export interface PostSignin {
+  email: string;
+  password: string;
+}
