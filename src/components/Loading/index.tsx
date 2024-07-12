5a75877753b27c9c59160ccd051ea14cfb691adc
MoonPillGyeom <ansvkf0820@naver.com>
2024-07-12 13:13:21 +0900

    feat: 로딩 컴포넌트 생성

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <>
+      <div className="h-400 w-500 bg-red-300">
+        <div className="text-9xl text-white">로딩!!</div>
+      </div>
+    </>
+  );
+}
