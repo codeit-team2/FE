71086b82029a6229481c5531d41eee940e04ca05
JunCastle <juncastle97@gmail.com>
2024-07-12 17:02:42 +0900

    fix : 애니메이션 수정

--- src/components/common/Bookmark/index.tsx
+++ src/components/common/Bookmark/index.tsx
@@ -12,17 +12,16 @@ export default function Bookmark() {
   return (
     <button type="button" onClick={handleClick}>
       <div
-        className={`relative flex h-40 w-40 items-center justify-center transition-transform duration-300 ease-in md:h-48 md:w-48 ${!isBookmarked ? '' : 'animate-pulseSmall'}`}
+        className={`relative transition-transform duration-300 ease-in ${!isBookmarked ? '' : 'animate-pulseSmall'}`}
       >
-        <Image
-          src={isBookmarked ? '/icons/ic-heart-bg-on.svg' : '/icons/ic-heart-bg-off.svg'}
-          alt="찜 버튼 배경"
-          layout="fill"
-        />
-        <div className="relative h-18 w-20 transition-all duration-300 ease-in md:h-22 md:w-24">
-          <Image src="/icons/ic-heart-off.svg" alt="찜 버튼 하트 (꺼짐)" layout="fill" />
-          <div className={`absolute ${isBookmarked ? 'animate-fillHeart inset-0' : ''}`}>
-            <Image src="/icons/ic-heart-on.svg" alt="찜 버튼 하트 (켜짐)" layout="fill" />
+        <div
+          className={`flex h-40 w-40 items-center justify-center rounded-full border-2 md:h-48 md:w-48 ${!isBookmarked ? 'border-neutral-100 bg-neutral-50' : 'border-secondary-100 bg-secondary-100'}`}
+        >
+          <div className="relative h-18 w-20 transition-all duration-300 ease-in md:h-22 md:w-24">
+            <Image src="/icons/ic-heart-off.svg" alt="찜 버튼 하트 (꺼짐)" layout="fill" />
+            <div className={`absolute ${isBookmarked ? 'animate-fillHeart inset-0' : ''}`}>
+              <Image src="/icons/ic-heart-on.svg" alt="찜 버튼 하트 (켜짐)" layout="fill" />
+            </div>
           </div>
         </div>
       </div>
