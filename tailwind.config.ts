8d89ccb7440dd986c4387d6cd2da1b83e9457df4
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-07-14 22:07:28 +0900

    chore: tailwind.config.ts에서 tailwind scrollbar 삭제

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -245,7 +245,7 @@ const config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar')],
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
 
 export default config;
