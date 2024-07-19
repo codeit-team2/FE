b9e443d5cd1f91a21f20ab8a9a3a20c6d0d035c7
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-07-19 17:27:39 +0900

    feat: accessToken 만료 후 재발급

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,16 +1,33 @@
+import { postRefreshtoken } from '@/apis/auths';
+
 import axios from 'axios';
+import { deleteCookie, getCookie, setCookie } from 'cookies-next';
 
 export const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
-    // Authorization: `Bearer ${auth.accessToken}`,
   },
   withCredentials: true,
 });
 
 instance.interceptors.request.use(
-  async (config) => config,
+  async (config) => {
+    const accessToken = getCookie('accessToken');
+    const refreshToken = getCookie('refreshToken');
+    if (!accessToken && refreshToken) {
+      const result = await postRefreshtoken();
+      deleteCookie('accessToken');
+      setCookie('accessToken', result.accessToken, {
+        path: '/',
+        maxAge: 12 * 60 * 60,
+      });
+    }
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
   (error) => Promise.reject(error),
 );
 
