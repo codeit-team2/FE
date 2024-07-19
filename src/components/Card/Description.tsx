ab767d812c3ff8107987142316769b22fb614f08
marchfirst01 <marchfirst3113@gmail.com>
2024-07-19 17:06:48 +0900

    feat: Card, Description Type 정리

--- src/components/Card/Description.tsx
+++ src/components/Card/Description.tsx
@@ -1,34 +1,29 @@
 import useFormatDate from '@/hooks/useFormatDate';
 
-interface Props {
-  data: Data;
-}
+import { Gathering } from '@/types/gatherings';
 
-interface Data {
-  category: string;
-  place: string;
-  date: string;
-  title: string;
-  deadline: string;
+interface Props {
+  data: Gathering;
 }
 
 export default function Description({ data }: Props) {
-  const formattedDate = useFormatDate({ date: data.date });
+  const formattedDate = useFormatDate({ date: data.dateTime });
 
   return (
     <div className="mb-16 flex w-full flex-col text-body-3Sb md:mb-4 md:text-body-2Sb">
       <div className="flex gap-6">
-        <p className="text-primary-300">{data.category}</p>
-        <p className="text-neutral-500">{data.place}</p>
+        <p className="text-primary-300">{data.subCategoryName}</p>
+        <p className="text-neutral-500">{data.location}</p>
       </div>
       <div className="mb-8 flex lg:gap-6">
-        <p className="text-secondary-300">{data.deadline}</p>·
+        {/* 작업필요 */}
+        {/* <p className="text-secondary-300">{data.deadline}</p>· */}
         <p className="text-neutral-500">
           {formattedDate?.formattedDate} {formattedDate?.formattedWeekday} ·{' '}
           {formattedDate?.formattedTime}
         </p>
       </div>
-      <div className="w-220 min-w-230 text-body-1Sb md:text-heading-2M">{data.title}</div>
+      <div className="w-220 min-w-230 text-body-1Sb md:text-heading-2M">{data.name}</div>
     </div>
   );
 }
