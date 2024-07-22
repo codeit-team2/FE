c5aba3c6dbf1d7415be8ab53d62d9522c0d9ecc6
MoonPillGyeom <ansvkf0820@naver.com>
2024-07-22 11:35:12 +0900

    feat: getGathering api 설정

--- src/apis/gatherings.ts
+++ src/apis/gatherings.ts
@@ -1,5 +1,7 @@
 import { instance } from '@/lib/axios';
 
+import { Gathering } from '@/types/gathering';
+
 // // export const getGatheringsList = async (value) => {
 // //   const res = await instance.get(
 // //     '/gatherings/info?mainCategory={모임 상위 카테고리}&subCategory={모임 하위 카테고리}&location={장소}&datetime={모임 날짜}&page={페이지}&size={한 페이지에 출력할 개수}&sortBy={정렬 기준}&sortOrder={오름차순/내림차순}',
@@ -20,9 +22,16 @@ import { instance } from '@/lib/axios';
 // //   );
 // //   return res.data;
 // // };
-
-export const getGatherings = async (gatheringId: number) => {
-  const res = await instance.get(`/gatherings/${gatheringId}`);
+// https://hobbyzone.p-e.kr/gatherings?page=0&size=5&sortBy=dateTime&sortOrder=asc
+export const getGatherings = async (
+  page: string = '0',
+  size: string = '5',
+  sortBy: string = 'dateTime',
+  sortOrder: string = 'asc',
+) => {
+  const res = await instance.get(
+    `/gatherings?page=${page}&size=${size}&sortBy=${sortBy}&sortOrder=${sortOrder}`,
+  );
   return res.data;
 };
 
