d2bb60961b9f4928286b0103b8671285bd5c4a07
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-07-26 16:13:14 +0900

    feat: focus될 때 outline 삭제

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -7,7 +7,7 @@ import { type VariantProps, cva } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none',
+  'inline-flex items-center justify-center whitespace-nowrap ring-offset-background transition-colors focus:outline-none disabled:pointer-events-none',
   {
     variants: {
       variant: {
