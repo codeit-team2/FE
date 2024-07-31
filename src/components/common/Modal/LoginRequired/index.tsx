d1bf2a777c548533714ed40676b0b00683be6d0b
JunCastle <juncastle97@gmail.com>
2024-07-31 14:40:28 +0900

    fix : 메인 카드 참여하기 버튼 수정

--- src/components/common/Modal/LoginRequired/index.tsx
+++ src/components/common/Modal/LoginRequired/index.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 
 import Image from 'next/image';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import { Button } from '@/components/ui/button';
 
 interface LoginRequiredProps {
-  // build에러로 옵서녈처리
   onClose?: () => void;
 }
 
 export default function LoginRequired({ onClose }: LoginRequiredProps) {
+  const router = useRouter();
+
+  const handleLoginClick = () => {
+    if (onClose) {
+      onClose();
+    }
+    router.push('/');
+  };
+
   return (
     <>
       <div className="fixed left-1/2 top-1/2 z-50 flex h-252 w-320 -translate-x-1/2 -translate-y-1/2 transform justify-center rounded-md bg-white p-20 md:h-332 md:w-520 md:px-40 md:py-32">
@@ -19,8 +27,12 @@ export default function LoginRequired({ onClose }: LoginRequiredProps) {
             <Image src={'/images/login.png'} alt="login-required" fill />
           </div>
           <p className="text-body-1Sb text-neutral-900 md:text-heading-2Sb">로그인이 필요해요</p>
-          <Button variant={'secondary'} className="w-280 text-body-1Sb md:w-440">
-            <Link href={'/'}>로그인하러 가기</Link>
+          <Button
+            variant={'secondary'}
+            className="w-280 text-body-1Sb md:w-440"
+            onClick={handleLoginClick}
+          >
+            로그인하러 가기
           </Button>
           <button className="absolute right-0 top-0" onClick={onClose}>
             <Image src={'/icons/ic-close.svg'} alt="closeicon" width={32} height={32} />
