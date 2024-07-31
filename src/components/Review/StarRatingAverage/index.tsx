ca2ae3ea24b0eae40a0cd4cfca910cfc0cc050bb
JunCastle <juncastle97@gmail.com>
2024-07-31 18:19:38 +0900

    fix : 폰트 색상 수정

--- src/components/Review/StarRatingAverage/index.tsx
+++ src/components/Review/StarRatingAverage/index.tsx
@@ -60,7 +60,7 @@ export default function StarRatingAverage({ data }: StarRatingAverageProps) {
                     style={{ width: `${progressPercentage}%` }}
                   />
                 </div>
-                <p>{score}</p>
+                <p className="text-neutral-400">{score}</p>
               </div>
             </div>
           );
