dfc3598a1c6627066dcacce5b1bfae09d49ccb8f
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-08-01 14:40:10 +0900

    feat: contextAPI 사용하여 로그인 상태 관리

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { AuthProvider } from '@/context/AuthProvider';
 import '@/styles/globals.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
@@ -13,10 +14,12 @@ const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <ErrorBoundary fallback={<div>ERROR!!!!!</div>}> */}
-      <Component {...pageProps} />
-      {/* </ErrorBoundary> */}
-      <ReactQueryDevtools initialIsOpen={false} />
+      <AuthProvider>
+        {/* <ErrorBoundary fallback={<div>ERROR!!!!!</div>}> */}
+        <Component {...pageProps} />
+        {/* </ErrorBoundary> */}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </AuthProvider>
     </QueryClientProvider>
   );
 }
